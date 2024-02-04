c92e0857fee5adf5908afe04f537697dbc2eaa5c
jsayev <cisayev@gmail.com>
2024-02-04 22:02:08 +0100

    Make drawForMatch protected

--- src/game/card/crazy/eights/Player.java
+++ src/game/card/crazy/eights/Player.java
@@ -34,7 +34,7 @@ class Player {
         return score;
     }
 
-    private Card drawForMatch(Eights eights, Card prev) {
+    protected Card drawForMatch(Eights eights, Card prev) {
         while (true) {
             Card card = eights.drawCard();
             System.out.println(name + " draws " + card);
